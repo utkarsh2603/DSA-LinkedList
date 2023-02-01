2e982ab132279ed40057adf7b8de87c7ba8dffb1
utkarsh2603 <utkarsh70888@gmail.com>
2023-02-01 10:05:07 +0530

    Initial Commit

--- Node_Class_Creation.java
+++ Node_Class_Creation.java
@@ -1,4 +1,23 @@
 public class Node_Class_Creation
 {
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String args[])
+    {
+
+    }
 
 }
