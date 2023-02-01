64c62023890ea1e229807e914c354216c3252a99
utkarsh2603 <utkarsh70888@gmail.com>
2023-02-01 14:30:56 +0530

    Initial Commit

---
+++ Search_Recursive.java
@@ -0,0 +1,173 @@
+public class Search_Recursive
+{
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //methods--ADD FIRST IN LINKEDLIST
+
+    public void addFirst(int data) {
+        //STEP-1
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        //STEP-2
+        newNode.next = head;
+        //STEP-3
+        head = newNode;
+    }
+
+    //Methods-ADD Last in LinkedList
+    public void addLast(int data) {
+        //STEP-1
+        Node newNode = new Node(data);
+        size++;
+
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //STEP-2
+        tail.next = newNode;
+
+        //STEP-3
+        tail = newNode;
+    }
+
+    //Method---Printing Linked list
+    public void print() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+        }
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    //Method---Adding element in middle
+    public void add(int index, int data) {
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    //Method---Removing first element
+
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        } else {
+            int val = head.data;
+            head = head.next;
+            size--;
+            return val;
+        }
+    }
+
+    //Method---Removing last element
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < size - 2; i++) {
+                prev = prev.next;
+            }
+            int val = prev.next.data;
+            prev.next = null;
+            tail = prev;
+            size--;
+            return val;
+        }
+    }
+
+
+    public int itr_Search(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public int helper(Node head,int key)
+    {
+        if(head==null)
+        {
+            return -1;
+        }
+        if(head.data==key)
+        {
+            return 0;
+        }
+        int idx=helper(head.next,key);
+        if(idx==-1)
+        {
+            return -1;
+        }
+        return idx+1;
+    }
+
+    public int recursive_Search(int key)
+    {
+        return helper(head,key);
+    }
+
+
+    public static void main(String args[])
+    {
+        Search_Recursive ll=new Search_Recursive();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        int key=5;
+        System.out.println(ll.recursive_Search(key));
+
+    }
+}
